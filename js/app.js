4508054af1d5967daeff15e97bc61d44fcea6b75
Leonardo Fernandez <leonardo.fernandez1402@gmail.com>
2020-06-28 18:48:46 -0300

    load time improvement, changes in the presentation and the stylesheets, added a function in Javascript for small devices

--- js/app.js
+++ js/app.js
@@ -20,5 +20,24 @@
     var scroll = new SmoothScroll('a[href*="#"]', {
       speed: 1500,
     });
+    // video android
+    document.querySelector("#video").play();
+    // mobile responsive projects
+    if (window.matchMedia("(min-width: 768px)")) {
+      const project = Array.from(document.getElementsByClassName("project"));
+      function projectLinks(e) {
+        var projectLink = e.target.parentElement.childNodes[3];
+        projectLink.classList.add("display-flex");
+        setTimeout(() => {
+          projectLink.classList.remove("display-flex");
+        }, 5000);
+      }
+
+      project.map(function (pro) {
+        pro.addEventListener("click", projectLinks);
+      });
+
+      // project.addEventListener("click", projectLinks);
+    }
   });
 })();
